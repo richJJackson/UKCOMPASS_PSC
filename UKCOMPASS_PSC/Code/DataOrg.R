0479c5b9919a86cab8fc67650e6acf8f6faf7dd3
richJJackson <richj23@liverpool.ac.uk>
2024-09-18 11:28:35 +0100

    Initial Analysis

--- UKCOMPASS_PSC/Code/DataOrg.R
+++ UKCOMPASS_PSC/Code/DataOrg.R
@@ -9,11 +9,8 @@ setwd("Data")
 ### laoding data
 load("ukc_ri.r")
 
-ri_late
-
 ## Defining outcome
 which(!is.na(ri$late_opDt))
-
 
 
 ri_late$opTm <- as.numeric(as.Date(ri_late$late_opDt,"%Y-%m-%d")-as.Date(ri_late$T0,"%d%b%Y"))/30.44
@@ -22,19 +19,11 @@ ri_late$out <- ((!is.na(ri_late$opTm))|ri_late$cen==1)
 ri_late$out[which(is.na(ri_late$out))] <- 0
 ri_late$t2 <- factor(ri_late$tx,labels=c("OR","EV","EV"))
 
+ri_late$I_sex <- factor(ri_late$I_sex)
+ri_late$asa_cat <- factor(ri_late$asa_cat)
 
+data <- ri_late
+save(data,file="analData.R")
 
-
-# PSC
-data_or  <- ri_late[which(ri_late$t2=="OR"),]
-model <- glm(out~factor(I_sex)+diameter+factor(asa2)+(age)+htn+neck_lgth+bmi+i_crea,data=data_or,family="binomial")
-
-summary(model)
-anova(model)
-
-data_or[1:3,]
-
-modelri_late$asa2 <- factor(ri_late$asa,labels=c(1,1,3,3,3))
-table(data_or$out,data_or$asa2)
 
 
